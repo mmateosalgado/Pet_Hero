f5e42cb678bda9f8f01a40fb4d49fd48b4525a11
Mateo Salgado <salgadomateo001@gmail.com>
2022-10-03 10:06:40 -0300

    -Creacion de carpetas     -Agregue lo que teniamos de views

---
+++ Views/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <LINK REL=StyleSheet HREF="css/style.css" TYPE="text/css" MEDIA=screen>
+    </head>
+    <body>
+    <div class="body"></div>
+            <div class="grad"></div>
+            <div class="header">
+                <div>Pet<span>Hero</span></div>
+            </div>
+            <br>
+            <div class="login">
+                    <input type="text" placeholder="username" name="user"><br>
+                    <input type="password" placeholder="password" name="password"><br>
+                    <input type="button" value="Login">
+            </div>
+    </body>
+</html>
