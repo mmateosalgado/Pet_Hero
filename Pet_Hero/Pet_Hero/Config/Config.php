f10a0acc58eff3b04dccc226b0c8197ba18a3677
romucurrao <romucurrao@gmail.com>
2022-10-23 23:32:02 -0300

    LISTOO

--- Pet_Hero/Pet_Hero/Config/Config.php
+++ Pet_Hero/Pet_Hero/Config/Config.php
@@ -2,11 +2,7 @@
 
     define("ROOT", dirname(__DIR__) . "/");
     //Path to your project's root folder
-<<<<<<< HEAD
-    define("FRONT_ROOT", "/1Actividades/Pet_Hero/Pet_Hero/Pet_Hero/");
-=======
     define("FRONT_ROOT", "/Ejercicios/Pet_Hero/Pet_Hero/Pet_Hero/");
->>>>>>> 05169ce1ce69bc8e1823f53e3d07644702e4ae38
     define("IMG_ROOT",ROOT."Views/Images");
     //EL ROOT DE ROMU:  /1Actividades/Pet_Hero/Pet_Hero/Pet_Hero/
     //EL ROOT DE SALGA: /FACULTAD XAMP/PET_HERO/Pet_Hero/Pet_Hero/Pet_Hero/
