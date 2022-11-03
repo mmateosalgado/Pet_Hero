3d5358ae96109ab4481d7b329c8a4e0800202b6c
Mateo (Mathew) Salgado <88723330+mmateosalgado@users.noreply.github.com>
2022-11-03 12:30:11 -0300

    -En el duenio , modificacmos el sistema de busqueda de guardianes por fecha disponible para que funcione con el sistema de fechas en arreglos.

--- Pet_Hero/Pet_Hero/Config/Config.php
+++ Pet_Hero/Pet_Hero/Config/Config.php
@@ -11,6 +11,11 @@
     define("CSS_PATH", FRONT_ROOT.VIEWS_PATH . "css/");
     //define("JS_PATH", FRONT_ROOT.VIEWS_PATH . "js/");
     define("IMG_PATH", FRONT_ROOT.VIEWS_PATH . "Images/");
+
+    define("DB_HOST", "localhost");
+    define("DB_NAME", "Pet_Hero");
+    define("DB_USER", "root");
+    define("DB_PASS", "");
 ?>
 
 
