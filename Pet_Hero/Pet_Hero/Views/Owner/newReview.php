57c4aadccdb6352b073b6dd93fc9a3b3277ac524
Facundo Ferrari <88807740+Facu-Ferra@users.noreply.github.com>
2022-11-03 09:52:11 -0300

    aaa

---
+++ Pet_Hero/Pet_Hero/Views/Owner/newReview.php
@@ -0,0 +1,26 @@
+<form>
+<div class="header">
+                <div>Rev<span>iew</span></div>
+            </div>
+            <br>
+            <div class="addPetForm addReview">
+                <br>
+                <label> Calificación:</label>
+                <p class="clasificacion">
+                    <input id="radio1" type="radio" name="estrellas" value="5"><!--
+                    --><label for="radio1">★</label><!--
+                    --><input id="radio2" type="radio" name="estrellas" value="4"><!--
+                    --><label for="radio2">★</label><!--
+                    --><input id="radio3" type="radio" name="estrellas" value="3"><!--
+                    --><label for="radio3">★</label><!--
+                    --><input id="radio4" type="radio" name="estrellas" value="2"><!--
+                    --><label for="radio4">★</label><!--
+                    --><input id="radio5" type="radio" name="estrellas" value="1"><!--
+                    --><label for="radio5">★</label>
+                </p><br>
+                    <label > Reseña del Guardian: </label><br>
+                    <textarea placeholder="Preguntale a tu amiguito animal como la paso!" form="*" name="review"></textarea><br><br>
+            
+                    <input type="submit" value="Enviar"><br>
+            </div>
+</form>
